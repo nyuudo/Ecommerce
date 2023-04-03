d7822e1eadd9935fa718888c996e58da679604a8
nyuudo <hello@nyuudo.com>
2023-04-03 17:03:50 +0200

    Exercise 1 + updates to accept modules

--- js/shop.js
+++ js/shop.js
@@ -1,125 +1,82 @@
-// If you have time, you can move this variable "products" to a json or js file and load the data in this js. It will look more professional
-var products = [
-   {
-        id: 1,
-        name: 'cooking oil',
-        price: 10.5,
-        type: 'grocery',
-        offer: {
-            number: 3,
-            percent: 20
-        }
-    },
-    {
-        id: 2,
-        name: 'Pasta',
-        price: 6.25,
-        type: 'grocery'
-    },
-    {
-        id: 3,
-        name: 'Instant cupcake mixture',
-        price: 5,
-        type: 'grocery',
-        offer: {
-            number: 10,
-            percent: 30
-        }
-    },
-    {
-        id: 4,
-        name: 'All-in-one',
-        price: 260,
-        type: 'beauty'
-    },
-    {
-        id: 5,
-        name: 'Zero Make-up Kit',
-        price: 20.5,
-        type: 'beauty'
-    },
-    {
-        id: 6,
-        name: 'Lip Tints',
-        price: 12.75,
-        type: 'beauty'
-    },
-    {
-        id: 7,
-        name: 'Lawn Dress',
-        price: 15,
-        type: 'clothes'
-    },
-    {
-        id: 8,
-        name: 'Lawn-Chiffon Combo',
-        price: 19.99,
-        type: 'clothes'
-    },
-    {
-        id: 9,
-        name: 'Toddler Frock',
-        price: 9.99,
-        type: 'clothes'
-    }
-]
+// Import Modules
+import { products } from "./products.js";
+
+// Listeners to access Product Buttons in html from Module
+const productButtons = document.querySelectorAll('[id^="product-"]');
+for (let i = 0; i < productButtons.length; i++) {
+  let id = productButtons[i].id.substring(9);
+  productButtons[i].addEventListener("click", () => buy(id));
+}
+
+// Listeners Functions in Module
+document.getElementById("open_modal").addEventListener("click", open_modal);
+document.getElementById("cleanCart").addEventListener("click", cleanCart);
+document
+  .getElementById("calculateTotal")
+  .addEventListener("click", calculateTotal);
+document.getElementById("generateCart").addEventListener("click", generateCart);
+document
+  .getElementById("applyPromotionsCart")
+  .addEventListener("click", applyPromotionsCart);
+
 // Array with products (objects) added directly with push(). Products in this array are repeated.
-var cartList = [];
+let cartList = [];
 
 // Improved version of cartList. Cart is an array of products (objects), but each one has a quantity field to define its quantity, so these products are not repeated.
-var cart = [];
+let cart = [];
 
-var total = 0;
-
+let total = 0;
 // Exercise 1
 function buy(id) {
-    // 1. Loop for to the array products to get the item to add to cart
-    // 2. Add found product to the cartList array
+  // 1. Loop for to the array products to get the item to add to cart
+  // 2. Add found product to the cartList array
+  for (let i = 0; i < products.length; i++) {
+    if (products[i].id == id) {
+      cartList.push(products[i]);
+      console.log(cartList);
+    }
+  }
 }
-
 // Exercise 2
-function cleanCart() {
-
-}
+function cleanCart() {}
 
 // Exercise 3
 function calculateTotal() {
-    // Calculate total price of the cart using the "cartList" array
+  // Calculate total price of the cart using the "cartList" array
 }
 
 // Exercise 4
 function generateCart() {
-    // Using the "cartlist" array that contains all the items in the shopping cart, 
-    // generate the "cart" array that does not contain repeated items, instead each item of this array "cart" shows the quantity of product.
+  // Using the "cartlist" array that contains all the items in the shopping cart,
+  // generate the "cart" array that does not contain repeated items, instead each item of this array "cart" shows the quantity of product.
 }
 
 // Exercise 5
 function applyPromotionsCart() {
-    // Apply promotions to each item in the array "cart"
+  // Apply promotions to each item in the array "cart"
 }
 
 // Exercise 6
 function printCart() {
-    // Fill the shopping cart modal manipulating the shopping cart dom
+  // Fill the shopping cart modal manipulating the shopping cart dom
 }
-
 
 // ** Nivell II **
 
 // Exercise 7
 function addToCart(id) {
-    // Refactor previous code in order to simplify it 
-    // 1. Loop for to the array products to get the item to add to cart
-    // 2. Add found product to the cart array or update its quantity in case it has been added previously.
+  // Refactor previous code in order to simplify it
+  // 1. Loop for to the array products to get the item to add to cart
+  // 2. Add found product to the cart array or update its quantity in case it has been added previously.
 }
 
 // Exercise 8
 function removeFromCart(id) {
-    // 1. Loop for to the array products to get the item to add to cart
-    // 2. Add found product to the cartList array
+  // 1. Loop for to the array products to get the item to add to cart
+  // 2. Add found product to the cartList array
 }
 
-function open_modal(){
-	console.log("Open Modal");
-	printCart();
+function open_modal() {
+  console.log("Open Modal");
+  printCart();
 }
